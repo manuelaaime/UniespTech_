2a06034cb1cd7b905c8d2d719b18f21648e1114a
manuelaaime <aimemanu28@gmail.com>
2026-03-10 14:49:31 -0300

    feat/manuela: adicionando o SpringBoot ao projeto

---
+++ src/test/java/com/uniesp/tech/demo/ProjetoUniespTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniesp.tech.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoUniespTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
